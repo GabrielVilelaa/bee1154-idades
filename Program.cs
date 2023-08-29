cf4216c117848407d50447638428e3dc4d8b2689
gabri <gabrielvilela.acer@gmail.com>
2023-08-29 07:22:27 -0400

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x= int.Parse (Console.ReadLine());
+
+
+            double sum = 0;
+            double cont = 0;
+            while (x >= 0)
+            {
+                sum = sum + x;
+                cont = cont + 1;
+                x = int.Parse(Console.ReadLine());
+            }
+            double resultado = (double) sum / cont;
+            Console.WriteLine(resultado.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
